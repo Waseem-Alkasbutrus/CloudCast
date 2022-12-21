9e36b2f752fd602bc627a2ba8fbbb3ed7e73f112
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2022-12-21 12:13:50 -0600

    Added gradients to search and settings screens

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import ScreenWrapper from '../components/ScreenWrapper'
 
 export default function SearchScreen() {
   return (
-    <SafeAreaView>
+    <ScreenWrapper>
       <Text>Search Screen</Text>
-    </SafeAreaView>
+    </ScreenWrapper>
   )
 }
--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,56 +1,72 @@
-import React from "react";
-import { View, StyleSheet, Alert } from "react-native";
+import React from 'react'
+import { View, StyleSheet, Alert } from 'react-native'
 
 import TitledSection from '../components/TitledSection'
-import { OpenDialogue } from "../components/SettingItem";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Button from "../components/Button";
+import { OpenDialogue } from '../components/SettingItem'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Button from '../components/Button'
+import ScreenWrapper from '../components/ScreenWrapper'
 
-buttonShouldDo = () => {console.log("button pressed")}
-
-const deleteSaves = () =>
-Alert.alert(
-  "Are you sure?",
-  "If you delete saves, we will not be able to recover them anymore",
-  [
-    {
-      text: "Cancel",
-      onPress: () => console.log("Cancel Pressed"),
-      style: "cancel"
-    },
-    { text: "Delete Saves", onPress: () => console.log("Items Deleted") }
-  ],
-  {
-    cancelable: true,
-  }
-);
-
-
-
-export default function SettingsScreen() {
-    return (
-        <SafeAreaView style={styles.settingsContainer}>
-            <TitledSection Label={'Privacy'}>
-                <OpenDialogue SettingName={'Location'} Description={'On'} Icon={require('../../assets/icons/Location.png')} />
-            </TitledSection>
-
-            <TitledSection Label={'Units'}>
-                <OpenDialogue SettingName={'Temperature'} Description={'Fahrenheit'} Icon={require('../../assets/icons/Thermometer.png')} />
-                <OpenDialogue SettingName={'Length'} Description={'Miles'} Icon={require('../../assets/icons/Ruler.png')} />
-                <OpenDialogue SettingName={'Pressure'} Description={'Atm'} Icon={require('../../assets/icons/Pressure.png')} />
-            </TitledSection>
-
-            <TitledSection Label={'Danger Zone'}>
-                <Button Label={"Delete Saved Data"} Action={deleteSaves}></Button>
-            </TitledSection>
-        </SafeAreaView>
-    );
+buttonShouldDo = () => {
+  console.log('button pressed')
 }
 
+const deleteSaves = () =>
+  Alert.alert(
+    'Are you sure?',
+    'If you delete saves, we will not be able to recover them anymore',
+    [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'Delete Saves', onPress: () => console.log('Items Deleted') },
+    ],
+    {
+      cancelable: true,
+    },
+  )
+
+export default function SettingsScreen() {
+  return (
+    <ScreenWrapper>
+      <TitledSection Label={'Privacy'}>
+        <OpenDialogue
+          SettingName={'Location'}
+          Description={'On'}
+          Icon={require('../../assets/icons/Location.png')}
+        />
+      </TitledSection>
+
+      <TitledSection Label={'Units'}>
+        <OpenDialogue
+          SettingName={'Temperature'}
+          Description={'Fahrenheit'}
+          Icon={require('../../assets/icons/Thermometer.png')}
+        />
+        <OpenDialogue
+          SettingName={'Length'}
+          Description={'Miles'}
+          Icon={require('../../assets/icons/Ruler.png')}
+        />
+        <OpenDialogue
+          SettingName={'Pressure'}
+          Description={'Atm'}
+          Icon={require('../../assets/icons/Pressure.png')}
+        />
+      </TitledSection>
+
+      <TitledSection Label={'Danger Zone'}>
+        <Button Label={'Delete Saved Data'} Action={deleteSaves}></Button>
+      </TitledSection>
+    </ScreenWrapper>
+  )
+}
 
 const styles = StyleSheet.create({
-    settingsContainer: {
-      backgroundColor: '#F7F7F7',
-      flexGrow: 1
-    },
-  })
+  settingsContainer: {
+    backgroundColor: '#F7F7F7',
+    flexGrow: 1,
+  },
+})
