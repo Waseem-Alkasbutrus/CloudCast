546d2c0073cf2ab365f008f69f84b513a670f2a5
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2022-12-30 13:57:22 -0600

    Added search for cities

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,12 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Pressable, StyleSheet, View, Image } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 import FeaturedCity from '../components/FeaturedCity'
 import ScreenWrapper from '../components/ScreenWrapper'
 import TitledSection from '../components/TitledSection.jsx'
 
+import { API_KEY } from '@env'
+import Font from '../components/Font'
+import { CitySearchListItem } from '../components/CityListItem'
+
+async function searchFor(city, setResults) {
+  await fetch(
+    'http://api.openweathermap.org/geo/1.0/direct?q=' +
+      city +
+      '&limit=16&appid=' +
+      API_KEY,
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      try {
+        let results
+
+        if (res.length > 0) {
+          results = res.map((city) => {
+            return (
+              <CitySearchListItem
+                key={city.lat * city.long}
+                City={city}
+              ></CitySearchListItem>
+            )
+          })
+        } else {
+          results = [
+            <Font style={styles.emptySearch}>no matching cities...</Font>,
+          ]
+        }
+
+        setResults(results)
+      } catch (err) {
+        console.log(err.message)
+      }
+    })
+}
+
+function renderResults(results) {
+  let res
+  if (results.length > 0) {
+    res = <Font>{results[0].name + ' (' + results[0].state + ')'}</Font>
+  } else {
+    res = <Font>No cities found</Font>
+  }
+  ;<TitledSection Label={'Search Results'}>{res}</TitledSection>
+}
+
 export default function SearchScreen() {
   const [searchCity, setSearchCity] = useState()
+  const [searchResults, setSearchResults] = useState([])
 
   return (
     <ScreenWrapper>
@@ -17,7 +66,9 @@ export default function SearchScreen() {
           placeholderTextColor={'#FBFBFB95'}
           returnKeyType="search"
           onChangeText={(newText) => setSearchCity(newText)}
-          onSubmitEditing={() => console.log(searchCity)}
+          onSubmitEditing={() => {
+            searchFor(searchCity, setSearchResults)
+          }}
         ></TextInput>
 
         <Pressable
@@ -31,14 +82,7 @@ export default function SearchScreen() {
         </Pressable>
       </View>
 
-      <TitledSection Label="Featured Cities">
-        <FeaturedCity></FeaturedCity>
-        <FeaturedCity></FeaturedCity>
-        <FeaturedCity></FeaturedCity>
-        <FeaturedCity></FeaturedCity>
-        <FeaturedCity></FeaturedCity>
-        <FeaturedCity></FeaturedCity>
-      </TitledSection>
+      <TitledSection Label="Search Results">{searchResults}</TitledSection>
     </ScreenWrapper>
   )
 }
@@ -72,5 +116,11 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     width: 24,
     height: 24,
+  },
+  emptySearch: {
+    fontSize: 20,
+    color: '#FBFBFB',
+    textAlign: 'center',
+    marginTop: 24,
   },
 })
