68ed464d1a8806c7fe1ab5029ea4bcd22587430c
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2022-12-30 11:30:58 -0600

    removed default weather info

--- src/components/CityDetailedItem.jsx
+++ src/components/CityDetailedItem.jsx
@@ -4,14 +4,8 @@ import Font from './Font'
 
 import Stat from './IconStat'
 
-function defaultWeather() {
-  return JSON.parse(
-    '{"coord":{"lon":96.727,"lat":33.006},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04n"}],"base":"stations","main":{"temp":6.64,"feels_like":-2.22,"temp_min":6.64,"temp_max":6.64,"pressure":1016,"humidity":21,"sea_level":1016,"grnd_level":561},"visibility":10000,"wind":{"speed":4.65,"deg":290,"gust":4.45},"clouds":{"all":100},"dt":1671477417,"sys":{"country":"CN","sunrise":1671496282,"sunset":1671532174},"timezone":28800,"id":1280653,"name":"Machang","cod":200}',
-  )
-}
-
 export default function CityDetailedItem({
-  Weather = defaultWeather(),
+  Weather,
   Action = () => {
     console.log(Weather)
   },
