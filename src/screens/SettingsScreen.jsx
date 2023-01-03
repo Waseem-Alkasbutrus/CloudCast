90d98152a613d010ca92d37961d63d59d465937f
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-01-02 19:21:40 -0600

    Location status shows in settings screen

--- src/screens/SettingsScreen.jsx
+++ src/screens/SettingsScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Alert, Linking } from 'react-native'
 
 import TitledSection from '../components/TitledSection'
@@ -9,6 +9,7 @@ import { SafeAreaScreenWrapper } from '../components/ScreenWrapper'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import Toast from 'react-native-toast-message'
 import { CustomToast } from '../components/CustomToast'
+import * as Location from 'expo-location'
 
 async function deleteSaves() {
   try {
@@ -24,7 +25,7 @@ async function deleteSaves() {
   }
 }
 
-const deleteSavesDialogue = () =>
+function deleteSavesDialogue() {
   Alert.alert(
     'Are you sure?',
     'If you delete saves, we will not be able to recover them anymore',
@@ -39,14 +40,32 @@ const deleteSavesDialogue = () =>
       cancelable: true,
     },
   )
+}
+
+async function checkLocation(setLocationStatus) {
+  let {status} = await Location.requestForegroundPermissionsAsync()
+
+  let codes = {
+    'granted' : 'on',
+    'denied' : 'off'
+  }
+
+  setLocationStatus(codes[status])
+}
 
 export default function SettingsScreen() {
+  const [LocationStatus, setLocationStatus] = useState()
+  
+  useEffect(() => {
+    checkLocation(setLocationStatus)
+  }, [])
+
   return (
     <SafeAreaScreenWrapper>
       <TitledSection Label={'Privacy'}>
         <OpenDialogue
           SettingName={'Location'}
-          Description={'On'}
+          Description={LocationStatus}
           Icon={require('../../assets/icons/Location.png')}
           Action={() => {
             Linking.openSettings()
