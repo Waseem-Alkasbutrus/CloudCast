2ed58c4f96dfed01aaac98b84852b326adbce364
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-01-08 19:12:36 -0600

    Added loading bars for home screen

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -11,14 +11,20 @@ import Font from '../components/Font'
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import * as Location from 'expo-location'
-import { StyleSheet, RefreshControl, ScrollView } from 'react-native'
+import {
+  StyleSheet,
+  RefreshControl,
+  ScrollView,
+  View,
+  ActivityIndicator,
+} from 'react-native'
 import { Colors, getUnitSystem } from '../components/GlobalVars'
 import { useFocusEffect } from '@react-navigation/native'
 
 async function getLocation(setGPSWeather) {
   let { status } = await Location.requestForegroundPermissionsAsync()
   if (status !== 'granted') {
-    setGPSWeather(undefined)
+    setGPSWeather(false)
     return
   }
 
@@ -41,7 +47,9 @@ async function getLocationWeather(setGPSWeather, location) {
       '&cnt=1',
   )
     .then((res) => res.json())
-    .then((wthr) => setGPSWeather(wthr))
+    .then((wthr) => {
+      setGPSWeather(wthr)
+    })
 }
 
 async function fetchBookmarkedWeather(setFavWeather) {
@@ -90,7 +98,7 @@ async function fetchData(setGPSWeather, setFavWeather) {
 export default function HomeScreen({ navigation }) {
   const [UnitSystem, setUnitSystem] = useState()
   const [GPSweather, setGPSWeather] = useState()
-  const [FavWeather, setFavWeather] = useState([])
+  const [FavWeather, setFavWeather] = useState()
   const [refreshing, setRefreshing] = useState(false)
 
   const onRefresh = useCallback(() => {
@@ -100,38 +108,50 @@ export default function HomeScreen({ navigation }) {
 
   useFocusEffect(
     useCallback(() => {
-      getUnitSystem().then(sys => setUnitSystem(sys))
+      getUnitSystem().then((sys) => setUnitSystem(sys))
       fetchData(setGPSWeather, setFavWeather)
     }, []),
   )
 
-  let citylistItems = (
-    <TitledSection Label={'Favorite Cities'}>
-      {FavWeather != undefined && FavWeather.length > 0 ? (
-        FavWeather.map((cityWeather) => {
-          return (
-            <CityListItem
-              key={cityWeather.city.name}
-              Weather={cityWeather}
-              UnitSystem={UnitSystem}
-              Action={() => {
-                navigation.navigate('City', {
-                  CityName: cityWeather.city.name,
-                  Lon: cityWeather.city.coord.lon,
-                  Lat: cityWeather.city.coord.lat,
-                })
-              }}
-            ></CityListItem>
-          )
-        })
-      ) : (
-        <Font style={styles.noFavsText}>No favorites added...</Font>
-      )}
-    </TitledSection>
-  )
+  let colors = Colors._z
+
+  let citylistItems
+  if (FavWeather != undefined && FavWeather.length > 0) {
+    citylistItems = FavWeather.map((cityWeather) => {
+      return (
+        <CityListItem
+          key={cityWeather.city.name}
+          Weather={cityWeather}
+          UnitSystem={UnitSystem}
+          Action={() => {
+            navigation.navigate('City', {
+              CityName: cityWeather.city.name,
+              Lon: cityWeather.city.coord.lon,
+              Lat: cityWeather.city.coord.lat,
+            })
+          }}
+        ></CityListItem>
+      )
+    })
+  } else if (FavWeather == undefined) {
+    citylistItems = (
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+          paddingTop: 24,
+        }}
+      >
+        <ActivityIndicator size={'small'} color={colors.text}></ActivityIndicator>
+      </View>
+    )
+  } else {
+    citylistItems = <Font style={styles.noFavsText}>No favorites added...</Font>
+  }
 
   let gps
-  if (GPSweather != undefined) {
+  if (GPSweather != undefined && GPSweather != false) {
     gps = (
       <CityDetailedItem
         Weather={GPSweather}
@@ -145,9 +165,23 @@ export default function HomeScreen({ navigation }) {
         }}
       ></CityDetailedItem>
     )
+  } else if (GPSweather === undefined) {
+    gps = (
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+          marginTop: 8,
+          paddingVertical: 64,
+          backgroundColor: colors.pressable,
+          borderRadius: 8,
+        }}
+      >
+        <ActivityIndicator size={'small'} color={colors.text}></ActivityIndicator>
+      </View>
+    )
   }
-
-  let colors = Colors._z
 
   return (
     <SafeAreaScreenWrapper>
@@ -163,7 +197,7 @@ export default function HomeScreen({ navigation }) {
         }
       >
         {gps}
-        {citylistItems}
+        <TitledSection Label={'Favorite Cities'}>{citylistItems}</TitledSection>
       </ScrollView>
     </SafeAreaScreenWrapper>
   )
