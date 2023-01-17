c59ee93567443cf391339296ad3e60f678120a4c
Waseem-Alkasbutrus <waseem_imad@yahoo.com>
2023-01-17 08:16:29 -0600

    New weather icons

--- src/components/WeatherIcon.jsx
+++ src/components/WeatherIcon.jsx
@@ -1,19 +1,28 @@
 import { Image } from "react-native";
 
 const images = {
-    '01': require('../../assets/icons/weather/01.png'),
-    '02': require('../../assets/icons/weather/02.png'),
-    '03': require('../../assets/icons/weather/03.png'),
-    '04': require('../../assets/icons/weather/04.png'),
-    '09': require('../../assets/icons/weather/09.png'),
-    '10': require('../../assets/icons/weather/10.png'),
-    '11': require('../../assets/icons/weather/11.png'),
-    '13': require('../../assets/icons/weather/13.png'),
-    '50': require('../../assets/icons/weather/50.png'),
+    '01d': require('../../assets/icons/weather/01d.png'),
+    '02d': require('../../assets/icons/weather/02d.png'),
+    '03d': require('../../assets/icons/weather/03d.png'),
+    '04d': require('../../assets/icons/weather/04d.png'),
+    '09d': require('../../assets/icons/weather/09d.png'),
+    '10d': require('../../assets/icons/weather/10d.png'),
+    '11d': require('../../assets/icons/weather/11d.png'),
+    '13d': require('../../assets/icons/weather/13d.png'),
+    '50d': require('../../assets/icons/weather/50d.png'),
+    '01n': require('../../assets/icons/weather/01n.png'),
+    '02n': require('../../assets/icons/weather/02n.png'),
+    '03n': require('../../assets/icons/weather/03n.png'),
+    '04n': require('../../assets/icons/weather/04n.png'),
+    '09n': require('../../assets/icons/weather/09n.png'),
+    '10n': require('../../assets/icons/weather/10n.png'),
+    '11n': require('../../assets/icons/weather/11n.png'),
+    '13n': require('../../assets/icons/weather/13n.png'),
+    '50n': require('../../assets/icons/weather/50n.png'),
 }
 
 export function getWeatherIconPath(Condition) {
-    return images[Condition.substring(0, 2)]
+    return images[Condition]
 }
 
 export default function WeatherIcon({Condition, style}) {
